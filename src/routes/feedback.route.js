eb672ba7a091aca38f39411d72cd5835fd32d79b
git-demo2 <vaibhavahir4456@gmail.com>
2024-06-25 21:21:14 +0530

    add routes

--- src/routes/feedback.route.js
+++ src/routes/feedback.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { createFeedback,getFeedbackForItem} from "../controllers/feedback.controller.js";
+import {
+  createFeedback,
+  getFeedbackForItem,
+  deleteFeedback,
+} from "../controllers/feedback.controller.js";
 
 const router = Router();
 
 router.route("/create-feedback/:id").post(createFeedback);
 router.route("/get-feedback/:itemId/:itemType").get(getFeedbackForItem);
-
+router.route("/delete-feedback/:id").delete(deleteFeedback);
 
 export default router;
